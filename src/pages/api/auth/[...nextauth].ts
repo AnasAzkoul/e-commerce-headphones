5e7646525889184bcc95345c7de591600eaa67ab
Anas azkoul <anas@Anass-MacBook-Air.local>
2023-04-10 14:00:03 +0200

    revert changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { AuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import {prisma} from '@/lib/DB';
